bbd66ec7f06679415893657aadb2580519d34f47
Sumana Harihareswara <sumanah@panix.com>
2013-11-11 17:03:17 -0500

    more work

--- africans.py
+++ africans.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python
+#encoding:utf-8
+# The point of this script is to take a giant list of 2126 names from the Dictionary of African Biography Oxford Reference, check which names do not have an English Wikipedia entry, and then spit out that resultant set.
 
 import requests
 
-starterlist= ['Kayibanda, Grégoire','Kayoya, Michel','Kazahendike, Urieta','Kazibwa, Specioza Wandira','Kebede Mikael','Keen, John','Keino, Kip','Kéita, Aoua','Keita, Fodéba','Keïta, Modibo','Keïta, Salif','Keïta, Seydou']
+# starting with a small hardcoded sample to see what works
+
+starterlist= [u'Kayibanda, Grégoire',u'Kayoya, Michel',u'Kazahendike, Urieta',u'Kazibwa, Specioza Wandira',u'Kebede Mikael',u'Keen, John',u'Keino, Kip',u'Kéita, Aoua',u'Keita, Fodéba',u'Keïta, Modibo',u'Keïta, Salif',u'Keïta, Seydou']
 
 # massage names to make them firstname lastname
 
-pagelist = []
-
-for elem in starterlist:
-    elem = elem.split(", ")
-    elem.reverse()
-    pagelist.append(" ".join(elem))
-
-print "pagelist: %s" % pagelist
-
-
-# massagednames = map(lambda elem:" ".join(elem.split(",").reverse()), starterlist)
-# gets a TypeError; figure it out later.
+def massagenames(namelist):
+    return map(lambda elem:" ".join(elem.split(", ")[::-1]), starterlist)
 
 # for each name, do a search to see whether the pages exist on english wikipedia
+# /w/api.php?action=query&prop=info&format=json&titles=Wikipedia%7CDave%20Coulier%7CAntarctica: if ["query"]["pages"] has a negative int like -1, -2, etc. as a key, and if a key within that dict has the value "missing" (value: ""), then the page is missing from enwiki
+
+def leftout(origlist, formattedlist):
+    unsung = []
+    for x, elem in enumerate(formattedlist):
+        payload = dict(title=elem)
+        r = requests.get("http://en.wikipedia.org/w/api.php?action=query&prop=info&format=json, data=payload")
+        if "-1" in r["query"]["pages"]:
+            if "missing" in r["query"]["pages"]["-1"]:
+                unsung.append(starterlist[x])
+    return unsung
 
 # spit out list of who is left out
+
+
+# (to find out who actually did have an entry, do a simple set operation for difference between set(pagelist) and set(starterlist)
+
