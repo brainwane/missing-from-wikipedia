43e1e73d6d341d09a35f090f969dc37858c8a8fb
Sumana Harihareswara <sumanah@panix.com>
2013-11-12 12:14:21 -0500

    better docs

--- africans.py
+++ africans.py
@@ -2,8 +2,9 @@
 #encoding:utf-8
 # Copyright 2013 Sumana Harihareswara
 # GPL
-# The point of this script is to take a giant list of 2126 names from the Dictionary of African Biography Oxford Reference, check which names do not have an English Wikipedia entry, and then spit out that resultant set.
-# Note: to find out who actually did have an entry, do a simple set operation for difference between set(pagelist) and set(starterlist)
+# The point of this script is to take a giant list of names from some source, check which names do not have an English Wikipedia entry, and then spit out that resultant set. You could trivially change the API call in 'leftout' to check a different wiki.
+# Please add your name to the User-Agent in the headers dict in 'leftout'.
+# Note: to find out who actually did have an entry, do a simple set operation for difference between the original namelist and the file that comes out at the end.
 
 # testlist= [u'Kayibanda, Grégoire',u'Kayoya, Michel',u'Kazahendike, Urieta',u'Kazibwa, Specioza Wandira',u'Kebede Mikael',u'Keen, John',u'Keino, Kip',u'Kéita, Aoua',u'Keita, Fodéba',u'Keïta, Modibo',u'Keïta, Salif',u'Keïta, Seydou']
 
@@ -27,12 +28,13 @@ def leftout(nametuples, resultfile):
     """return list of people who don't have pages on English Wikipedia
 
     for each name, do a search to see whether the page exists on english wikipedia
-    sample title that does not exist: Narrrgh
-    /w/api.php?action=query&prop=info&format=json&titles=Narrrgh: if ["query"]["pages"] has a negative int like -1, -2, etc. as a key, and if a key within that dict has the value "missing" (value: ""), then the page is missing from enwiki
-    TODO: use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once. Can use chunks of up to 50 titles in 1 query. Right now it is ~1200 names; as long as I do them in series with a maxlag param, I think that is fine.
+       Sample title that does not exist: Narrrgh
+       API call goes to: /w/api.php?action=query&prop=info&format=json&titles=Narrrgh&redirects=&maxlag=5
+    If ["query"]["pages"] has a negative int like -1, -2, etc. as a key, and if a key within that dict has the value "missing" (value: ""), then the page is missing from the wiki.
+    TODO: use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once. Can use chunks of up to 50 titles in 1 query. I've used this with a list of ~1200 names and haven't implemented this yet; as long as I do them in series with a maxlag param, I think that is fine.
     Currently accepts redirects as meaning the page exists. TODO: if the redirect is to a page that is NOT a biography (e.g., it redirects to the page for a war), then count that person as unsung."""
 
-    headers = {'User-Agent': 'Sumana Harihareswara prototype (http://github.com/brainwane) using Python requests library'}
+    headers = {'User-Agent': 'missing-from-wikipedia project, using Python requests library'}
     for namepair in nametuples:
         payload = dict(titles=namepair[1])
         r = requests.get("http://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&redirects=&maxlag=5", params=payload, headers=headers)
