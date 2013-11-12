00a3dfc38d45873d10ea3304a9e990482f20c642
Sumana Harihareswara <sumanah@panix.com>
2013-11-12 17:45:55 -0500

    run standalone, take command line args

--- missing.py
+++ missing.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 #encoding:utf-8
 # Copyright 2013 Sumana Harihareswara
-# GPL
-# The point of this script is to take a giant list of names from some source, check which names do not have an English Wikipedia entry, and then spit out that resultant set. You could trivially change the API call in 'leftout' to check a different wiki, and change the call to "run" to change the filenames.
+# Licensed under the GPL - see LICENSE
+# The point of this script is to take a giant list of names from some source file, check which names do not have an entry on one language's Wikipedia, and then spit out that resultant set to a file. The command-line options let you specify the wiki to check and the filenames to read and write.
 # Please add your name to the User-Agent in the headers dict in 'leftout'.
-# Note: to find out who actually did have an entry, do a simple set operation for difference between the original namelist and the file that comes out at the end.
+# Note: to find out who actually did have an entry on the wiki, do a simple set operation for difference between the original namelist and the file that comes out at the end.
 
 # test names include: Mazari, Abu ʿAbd Allah Muhammad al- ; Mlapa III; Andrade, Mário Pinto de; Bayram al-Khaʾmis, Mohamed; Be’alu Girma; Bédié, Henri-Konan; Obama, Barack, Sr.; Okwei
 
 import requests
 import codecs
+import sys
+
+(inputfile, wiki, outputfile) = sys.argv[1:]
 
 def getnamelist(filename):
     """Open the file and turn it into a list split up by newlines."""
@@ -25,27 +28,30 @@ def massagenames(nlist):
     return zip(nlist, mlist)
 
 def chunknames(tuplelist):
-    """a generator to yield up 50 name pairs at a time"""
+    """A generator to yield up 50 name pairs at a time.
+
+    We can use chunks of up to 50 titles in 1 API query, at least on English Wikipedia."""
     while tuplelist:
         yield tuplelist[:50]
         tuplelist = tuplelist[50:]
 
 def leftout(nametuples, resultfile):
-    """return list of people who don't have pages on English Wikipedia
+    """Return list of people who don't have pages on the wiki.
 
-    for each name, do a search to see whether the page exists on english wikipedia
+    For each name, do a check to see whether the page exists on the wiki (as specified via command-line argument).
        Sample title that does not exist: Narrrgh
        API call goes to: /w/api.php?action=query&prop=info&format=json&titles=Narrrgh&redirects=&maxlag=5
     If ["query"]["pages"] has a negative int like -1, -2, etc. as a key, and if a key within that dict has the value "missing" (value: ""), then the page is missing from the wiki.
-    We use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once. Can use chunks of up to 50 titles in 1 query.
+    We use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once.
     Currently accepts redirects as meaning the page exists. TODO: if the redirect is to a page that is NOT a biography (e.g., it redirects to the page for a war), then count that person as unsung."""
 
-    headers = {'User-Agent': 'missing-from-wikipedia project, using Python requests library'}
+    headers = {'User-Agent': 'missing-from-wikipedia project (https://raw.github.com/brainwane/missing-from-wikipedia/), using Python requests library'}
     g = chunknames(nametuples)
     for chunk in g:
         names = [x[1] for x in chunk]
         payload = dict(titles="|".join(names))
-        r = requests.get("http://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&redirects=&maxlag=5", params=payload, headers=headers)
+        URI = "http://%s.wikipedia.org/w/api.php?action=query&prop=info&format=json&redirects=&maxlag=5" % wiki
+        r = requests.get(URI, params=payload, headers=headers)
         for key in r.json()["query"]["pages"]:
             if "missing" in r.json()["query"]["pages"][key]:
                 outputfile(r.json()["query"]["pages"][key]["title"], resultfile)
@@ -63,4 +69,6 @@ def run(listfile, resultfile):
     querynames = massagenames(listofnames)
     leftout(querynames, resultfile)
 
-run("namelist.txt", "missing-people.txt")
+if __name__ == "__main__":
+    """Run as: ./missing.py input-filename Wikipedia-code output-filename"""
+    run(inputfile, outputfile)
