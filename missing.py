e34a3d8fb1dae2984ce2338513146f64844f6ff4
Sumana Harihareswara <sumanah@panix.com>
2013-11-12 16:47:24 -0500

    intro comment, removing needless import, better names

--- missing.py
+++ missing.py
@@ -2,14 +2,13 @@
 #encoding:utf-8
 # Copyright 2013 Sumana Harihareswara
 # GPL
-# The point of this script is to take a giant list of names from some source, check which names do not have an English Wikipedia entry, and then spit out that resultant set. You could trivially change the API call in 'leftout' to check a different wiki.
+# The point of this script is to take a giant list of names from some source, check which names do not have an English Wikipedia entry, and then spit out that resultant set. You could trivially change the API call in 'leftout' to check a different wiki, and change the call to "run" to change the filenames.
 # Please add your name to the User-Agent in the headers dict in 'leftout'.
 # Note: to find out who actually did have an entry, do a simple set operation for difference between the original namelist and the file that comes out at the end.
 
-# testlist= [u'Kayibanda, Grégoire',u'Kayoya, Michel',u'Kazahendike, Urieta',u'Kazibwa, Specioza Wandira',u'Kebede Mikael',u'Keen, John',u'Keino, Kip',u'Kéita, Aoua',u'Keita, Fodéba',u'Keïta, Modibo',u'Keïta, Salif',u'Keïta, Seydou']
+# test names include: Mazari, Abu ʿAbd Allah Muhammad al- ; Mlapa III; Andrade, Mário Pinto de; Bayram al-Khaʾmis, Mohamed; Be’alu Girma; Bédié, Henri-Konan; Obama, Barack, Sr.; Okwei
 
 import requests
-import pprint
 import codecs
 
 def getnamelist(filename):
@@ -21,7 +20,8 @@ def getnamelist(filename):
 
 def massagenames(nlist):
     """massage each name in a list to make it firstname lastname, return a list of old-and-new tuples"""
-    mlist= map(lambda elem:" ".join(elem.split(", ")[::-1]), nlist)
+    mlist = map(lambda elem:" ".join(elem.split(", ")[::-1]), nlist)
+    mlist = [name[1].replace("- ","-") for name in enumerate(mlist)] # dealing with names with "al-" & similar strings
     return zip(nlist, mlist)
 
 def chunknames(tuplelist):
@@ -37,7 +37,7 @@ def leftout(nametuples, resultfile):
        Sample title that does not exist: Narrrgh
        API call goes to: /w/api.php?action=query&prop=info&format=json&titles=Narrrgh&redirects=&maxlag=5
     If ["query"]["pages"] has a negative int like -1, -2, etc. as a key, and if a key within that dict has the value "missing" (value: ""), then the page is missing from the wiki.
-    TODO: use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once. Can use chunks of up to 50 titles in 1 query. I've used this with a list of ~1200 names and haven't implemented this yet; as long as I do them in series with a maxlag param, I think that is fine.
+    We use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once. Can use chunks of up to 50 titles in 1 query.
     Currently accepts redirects as meaning the page exists. TODO: if the redirect is to a page that is NOT a biography (e.g., it redirects to the page for a war), then count that person as unsung."""
 
     headers = {'User-Agent': 'missing-from-wikipedia project, using Python requests library'}
@@ -49,7 +49,7 @@ def leftout(nametuples, resultfile):
         for key in r.json()["query"]["pages"]:
             if "missing" in r.json()["query"]["pages"][key]:
                 outputfile(r.json()["query"]["pages"][key]["title"], resultfile)
-        print("just ran a chunk, yo")
+        # print("just ran a chunk, yo") # for debugging
 
 # spit out list of who is left out
 
@@ -63,4 +63,4 @@ def run(listfile, resultfile):
     querynames = massagenames(listofnames)
     leftout(querynames, resultfile)
 
-run("namelist.txt", "unsung2.txt")
+run("namelist.txt", "missing-people.txt")
