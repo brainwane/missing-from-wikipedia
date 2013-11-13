87c1fa08a707eaea8f1d919469de4b49b3b5f4a2
Sumana Harihareswara <sumanah@panix.com>
2013-11-13 18:51:11 -0500

    pep8 and removing unnecessary arg

--- missing.py
+++ missing.py
@@ -2,9 +2,16 @@
 #encoding:utf-8
 # Copyright 2013 Sumana Harihareswara
 # Licensed under the GPL - see LICENSE
-# The point of this script is to take a giant list of names from some source file, check which names do not have an entry on one language's Wikipedia, and then spit out that resultant set to a file. The command-line options let you specify the wiki to check and the filenames to read and write.
+# The point of this script is to take a giant list of names from some source
+# file, check which names do not have an entry on one language's Wikipedia,
+# and then spit out that resultant set to a file. The command-line options let
+# you specify the wiki to check and the filenames to read and write.
+
 # Please add your name to the User-Agent in the headers dict in 'leftout'.
-# Note: to find out who actually did have an entry on the wiki, do a simple set operation for difference between the original namelist and the file that comes out at the end.
+
+# Note: to find out who actually did have an entry on the wiki, do a simple set
+# operation for difference between the original namelist and the file that
+# comes out at the end.
 
 # test names include: Mazari, Abu ʿAbd Allah Muhammad al- ; Mlapa III; Andrade, Mário Pinto de; Bayram al-Khaʾmis, Mohamed; Be’alu Girma; Bédié, Henri-Konan; Obama, Barack, Sr.; Okwei
 
@@ -13,6 +20,7 @@ import codecs
 import sys
 from datetime import datetime
 
+
 def getnamelist(filename):
     """Open the file and turn it into a list split up by newlines."""
     with codecs.open(filename, encoding='utf-8') as f:
@@ -20,19 +28,22 @@ def getnamelist(filename):
         namelist = fstr.split("\n")
     return namelist
 
+
 def massagenames(nlist):
-    """massage each name in a list to make it firstname lastname, return a list of old-and-new tuples"""
-    mlist = map(lambda elem:" ".join(elem.split(", ")[::-1]), nlist)
-    mlist = [name[1].replace("- ","-") for name in enumerate(mlist)] # dealing with names with "al-" & similar strings
+    """Take list, make each name firstname lastname, then return a list of old-and-new tuples."""
+    mlist = map(lambda elem: " ".join(elem.split(", ")[::-1]), nlist)
+    mlist = [name[1].replace("- ", "-") for name in enumerate(mlist)]  # dealing with names with "al-" & similar strings
     return zip(nlist, mlist)
+
 
 def chunknames(tuplelist):
     """A generator to yield up 50 name pairs at a time.
 
-    We can use chunks of up to 50 titles in 1 API query, at least on English Wikipedia."""
+    Can send up to 50 titles in 1 API query, at least on English Wikipedia."""
     while tuplelist:
         yield tuplelist[:50]
         tuplelist = tuplelist[50:]
+
 
 def leftout(nametuples, wikipedia, resultfile):
     """Return list of people who don't have pages on the wiki.
@@ -58,10 +69,12 @@ def leftout(nametuples, wikipedia, resultfile):
 
 # spit out list of who is left out
 
+
 def outputfile(pagename, fname):
     with codecs.open(fname, encoding='utf-8', mode='a') as u:
         u.write(pagename)
         u.write("\n")
+
 
 def nameoutputfile(startfile):
     """Name the output file: the input filename plus timestamp.
@@ -69,11 +82,15 @@ def nameoutputfile(startfile):
     Will not overwrite output file unless run more than once per second."""
     base = startfile.split('.txt')[0]
     now = datetime.utcnow().isoformat('-')
-    now = now[:19].replace(':','-')
+    now = now[:19].replace(':', '-')
     return "%s-%s.txt" % (base, now)
 
-def ratio(missed, orig, origfile, wikipedia, out):
-    """Tell the user the percentage of people who do not have wiki pages about them.
+
+def ratio(missed, orig, origfile, wikipedia):
+    """Command-line output with stats and call to action.
+
+    Tell user the percentage of people who do not have wiki pages about them.
+    Suggest things to do about that.
 
     Takes the resultfile from leftout and the list from getnamelist."""
     with codecs.open(missed, encoding='utf-8', mode='r') as g:
@@ -83,7 +100,8 @@ def ratio(missed, orig, origfile, wikipedia, out):
     print """Your output file: %(outfile)s
 %(unsungnum)s people (%(pct).0f percent of the %(orignum)s people listed in %(infile)s) do not have %(lang)s.wikipedia.org pages about them.
 Change that: https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Countering_systemic_bias
-In your language: https://www.wikidata.org/wiki/Q4656680\n""" % {'outfile':out, 'unsungnum':int(a), 'pct':percentage, 'orignum':b, 'infile':origfile, 'lang':wikipedia}
+In your language: https://www.wikidata.org/wiki/Q4656680\n""" % {'outfile': missed, 'unsungnum': int(a), 'pct': percentage, 'orignum': b, 'infile': origfile, 'lang': wikipedia}
+
 
 def run():
     (inputfile, wiki) = sys.argv[1:]
@@ -91,7 +109,8 @@ def run():
     listofnames = getnamelist(inputfile)
     querynames = massagenames(listofnames)
     leftout(querynames, wiki, out)
-    ratio(out, listofnames, inputfile, wiki, out)
+    ratio(out, listofnames, inputfile, wiki)
+
 
 if __name__ == "__main__":
     """Run as: ./missing.py input-filename Wikipedia-code output-filename"""
