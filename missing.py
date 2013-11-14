ba12aaeaec3ee18ec08071b197190f2d268b3dc1
Marcus Ljungblad <marcus@ljungblad.nu>
2013-11-14 12:59:14 -0500

    Code review: mostly work on readability
    * Moved constants to beginning of file and named them
    * It was assumed filename ends with .txt. Changed to arbitrary .ext but it is still kind of hackish
    * Current time was not provided, using local datetime.now() instead
    * Refactored ratio, now called generate_statistics, to only return the percentage and created new function to print results

--- missing.py
+++ missing.py
@@ -21,6 +21,13 @@ import sys
 from datetime import datetime
 
 
+# Constants
+CHUNK_SIZE = 50 # Can send up to 50 titles in 1 API query, at least on English Wikipedia.
+DEFAULT_HEADERS = {
+    'User-Agent': 'missing-from-wikipedia project (https://github.com/brainwane/missing-from-wikipedia/), using Python requests library'
+}
+
+
 def getnamelist(filename):
     """Open the file and turn it into a list split up by newlines."""
     with codecs.open(filename, encoding='utf-8') as f:
@@ -29,23 +36,20 @@ def getnamelist(filename):
     return namelist
 
 
-def massagenames(nlist):
+def massagenames(names):
     """Take list, make each name firstname lastname, then return a list of old-and-new tuples."""
-    mlist = map(lambda elem: " ".join(elem.split(", ")[::-1]), nlist)
-    mlist = [name[1].replace("- ", "-") for name in enumerate(mlist)]  # dealing with names with "al-" & similar strings
-    return zip(nlist, mlist)
+    massaged = map(lambda elem: " ".join(elem.split(", ")[::-1]), names)
+    massaged = [name[1].replace("- ", "-") for name in enumerate(massaged)]  # dealing with names with "al-" & similar strings
+    return massaged
+
+def chunknames(names):
+    """A generator to yield up CHUNK_SIZE name pairs at a time."""
+    while names:
+        yield names[:CHUNK_SIZE]
+        names = names[CHUNK_SIZE:]
 
 
-def chunknames(tuplelist):
-    """A generator to yield up 50 name pairs at a time.
-
-    Can send up to 50 titles in 1 API query, at least on English Wikipedia."""
-    while tuplelist:
-        yield tuplelist[:50]
-        tuplelist = tuplelist[50:]
-
-
-def leftout(nametuples, wikipedia, resultfile):
+def leftout(massaged_names, wikipedia_language, resultfile):
     """Return list of people who don't have pages on the wiki.
 
     For each name, do a check to see whether the page exists on the wiki (as specified via command-line argument).
@@ -54,64 +58,70 @@ def leftout(nametuples, wikipedia, resultfile):
     If ["query"]["pages"] has a negative int like -1, -2, etc. as a key, and if a key within that dict has the value "missing" (value: ""), then the page is missing from the wiki.
     We use pipes, e.g. Narrgh|Call Me Maybe|NEVEREXISTS in titles= , to make multiple queries at once.
     Currently accepts redirects as meaning the page exists. TODO: if the redirect is to a page that is NOT a biography (e.g., it redirects to the page for a war), then count that person as unsung."""
-
-    headers = {'User-Agent': 'missing-from-wikipedia project (https://github.com/brainwane/missing-from-wikipedia/), using Python requests library'}
-    g = chunknames(nametuples)
-    for chunk in g:
-        names = [x[1] for x in chunk]
-        payload = dict(titles="|".join(names))
-        URI = "http://%s.wikipedia.org/w/api.php?action=query&prop=info&format=json&redirects=&maxlag=5" % wikipedia
-        r = requests.get(URI, params=payload, headers=headers)
-        for key in r.json()["query"]["pages"]:
-            if "missing" in r.json()["query"]["pages"][key]:
-                outputfile(r.json()["query"]["pages"][key]["title"], resultfile)
-        # print("just ran a chunk, yo") # for debugging
-
-# spit out list of who is left out
+    
+    for chunk in chunknames(massaged_names):
+        payload = dict(titles="|".join(chunk))
+        URI = "http://%s.wikipedia.org/w/api.php?action=query&prop=info&format=json&redirects=&maxlag=5" % wikipedia_language
+        request = requests.get(URI, params=payload, headers=DEFAULT_HEADERS)
+        for key in request.json()["query"]["pages"]:
+            if "missing" in request.json()["query"]["pages"][key]:
+                outputfile(request.json()["query"]["pages"][key]["title"], resultfile)
 
 
-def outputfile(pagename, fname):
-    with codecs.open(fname, encoding='utf-8', mode='a') as u:
-        u.write(pagename)
-        u.write("\n")
+def outputfile(pagename, filename):
+    with codecs.open(filename, encoding='utf-8', mode='a') as out_fd:
+        out_fd.write("%s\n" % pagename)
 
 
-def nameoutputfile(startfile):
+def nameoutputfile(name):
     """Name the output file: the input filename plus timestamp.
 
     Will not overwrite output file unless run more than once per second."""
-    base = startfile.split('.txt')[0]
-    now = datetime.utcnow().isoformat('-')
-    now = now[:19].replace(':', '-')
-    return "%s-%s.txt" % (base, now)
+    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    pos = name.rfind('.')
+    return "%s-%s.%s" % (name[:pos], now, name[pos+1:]) if pos > 0 else "%s-%s" % (name, now)
 
 
-def ratio(missed, orig, origfile, wikipedia):
+def generate_statistics(file_with_missing_entries, original_list_of_names):
     """Command-line output with stats and call to action.
 
     Tell user the percentage of people who do not have wiki pages about them.
     Suggest things to do about that.
 
     Takes the resultfile from leftout and the list from getnamelist."""
-    with codecs.open(missed, encoding='utf-8', mode='r') as g:
-        a = float(len(list(g)))
-    b = len(orig)
-    percentage = 100*(a/b)
+    with codecs.open(file_with_missing_entries, encoding='utf-8', mode='r') as fd:
+        number_of_missing_entries = len(list(fd))
+
+    number_of_original_entries = len(original_list_of_names)
+    percentage = 100 * (float(number_of_missing_entries) / float(number_of_original_entries))
+    return {'ratio': percentage, 'original': number_of_original_entries, 'missing': number_of_missing_entries}
+
+
+def print_results(input_file, file_with_missing_entries, wikipedia_language, stats):
     print """Your output file: %(outfile)s
 %(unsungnum)s people (%(pct).0f percent of the %(orignum)s people listed in %(infile)s) do not have %(lang)s.wikipedia.org pages about them.
 Change that: https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Countering_systemic_bias
-In your language: https://www.wikidata.org/wiki/Q4656680\n""" % {'outfile': missed, 'unsungnum': int(a), 'pct': percentage, 'orignum': b, 'infile': origfile, 'lang': wikipedia}
+In your language: https://www.wikidata.org/wiki/Q4656680\n""" % {
+        'outfile': file_with_missing_entries, 
+        'unsungnum': stats['missing'], 
+        'pct': stats['ratio'], 
+        'orignum': stats['original'], 
+        'infile': input_file, 
+        'lang': wikipedia_language}
 
 
 def run():
-    (inputfile, wiki) = sys.argv[1:]
-    out = nameoutputfile(inputfile)
-    listofnames = getnamelist(inputfile)
-    querynames = massagenames(listofnames)
-    leftout(querynames, wiki, out)
-    ratio(out, listofnames, inputfile, wiki)
+    (inputfilename, wikipedia_language) = sys.argv[1:]
+    outputfilename = nameoutputfile(inputfilename)
+    names = getnamelist(inputfilename)
+    querynames = massagenames(names)
+    leftout(querynames, wikipedia_language, outputfilename)
+    stats = generate_statistics(outputfilename, names)
+    print_results(inputfilename, outputfilename, wikipedia_language, stats)
 
 
 if __name__ == "__main__":
-    """Run as: ./missing.py input-filename Wikipedia-code output-filename"""
-    run()
+    if len(sys.argv) > 2:
+        run()
+    else:
+        print "Usage: ./%s <input-filename> <wikipedia-language-code>" % sys.argv[0]
