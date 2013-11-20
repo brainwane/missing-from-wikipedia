c7a1eeb5ce46a97fc4d98f5f7bda14bf7b8b7916
Sumana Harihareswara <sumanah@panix.com>
2013-11-20 17:51:39 -0500

    basic check and display works

--- webapp/application.py
+++ webapp/application.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request
+import missing
+
 app = Flask(__name__)
 
-app.vars = {}
+# take in names from datainput.html form
+# massagenames
+#### chunknames - someday! when I actually have a bunch
+# leftout
+# return result to user in results.html form
+
+def onWikipedia(name):
+    names = [name]
+    names = missing.massagenames(names)
+    resultlist = missing.leftout(names, "en")
+    return resultlist != []
 
 @app.route('/index',methods=['GET','POST']) # form in template
 def index():
     if request.method == 'GET':
         return render_template('datainput.html')
     else:  # request was POST
-        app.vars['name'] = request.form['pagename']
-        return render_template('results.html',checkname=app.vars['name'])
+        nametocheck = request.form['pagename']
+        checkresult = onWikipedia(nametocheck)
+        return render_template('results.html', checkname=nametocheck, result=checkresult)
 
 if __name__ == "__main__":
     app.run(debug=True)
