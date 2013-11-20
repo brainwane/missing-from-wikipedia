aa77ec87d08dce20f4d8722b41035e25d4019ecc
Sumana Harihareswara <sumanah@panix.com>
2013-11-20 18:19:14 -0500

    carriage returns deserve a split

--- webapp/application.py
+++ webapp/application.py
@@ -4,25 +4,23 @@ import missing
 app = Flask(__name__)
 
 # take in names from datainput.html form
-# massagenames
-#### chunknames - someday! when I actually have a bunch
-# leftout
+# run massagenames (implicitly chunks into 50 titles per request) and leftout
 # return result to user in results.html form
 
-def onWikipedia(names):
-    names = names.split('\n')
+def onWikipedia(names, lang):
+    names = names.split('\r\n')
     names = missing.massagenames(names)
-    resultlist = missing.leftout(names, "en")
-    return resultlist
+    resultlist = missing.leftout(names, lang)
+    return names, resultlist
 
 @app.route('/index',methods=['GET','POST']) # form in template
 def index():
     if request.method == 'GET':
         return render_template('datainput.html')
     else:  # request was POST
-        namestocheck = request.form['pagename']
-        checkresult = onWikipedia(namestocheck)
-        return render_template('results.html', checkname=namestocheck, result=checkresult)
+        namestocheck, language = request.form['pagename'], request.form['langname']
+        orig, checkresult = onWikipedia(namestocheck, language)
+        return render_template('results.html', checkname=orig, result=checkresult)
 
 if __name__ == "__main__":
     app.run(debug=True)
