59fc5e69842cd05754ac24fac19ea68d9021ad9a
MichaelRochlin <michaelrochlin@gmail.com>
2013-11-24 14:31:59 -0500

    Fixed tests

--- tests.py
+++ tests.py
@@ -5,15 +5,16 @@
 
 
 import unittest
+import os
 from missing import *
 
 
 class input_processing_test(unittest.TestCase):
     def test_fileopen(self):
     # Test that the list splitting-up works.
-        testfile = "namelist-sample.txt"
+        self.testfile = "namelist-sample.txt"
         expectedresult = [u"Mazari, Abu ʿAbd Allah Muhammad al-", u"Mlapa III", u"Andrade, Mário Pinto de", u"Bayram al-Khaʾmis, Mohamed", u"Be’alu Girma", u"Bédié, Henri-Konan", u"Obama, Barack, Sr.", u"Okwei", u"Marie Curie", u"Cleopatra", u"Gandhi, Indira", u"Madikizela-Mandela, Winnie"]
-        testresult = getnamelist(testfile)
+        testresult = getnamelist(self.testfile)
         self.assertEqual(testresult, expectedresult)
 
 
@@ -28,34 +29,37 @@ class name_processing_test(unittest.TestCase):
     def test_three_item_reversal(self):
     # Test that special three-name items reverse appropriately.
         testname = ["Obama, Barack, Sr."]
-        expectedresult = ["Barack Obama Sr."]
+        expectedresult = ["Barack Obama, Sr."]
         testresult = massagenames(testname)
         self.assertEqual(testresult, expectedresult)
 
 
 class page_existence_test(unittest.TestCase):
+    def setUp(self):
+        self.testfile = "empty.txt"
+
     def test_existing_page(self):
     # Check that we know an existing page exists.
     # leftout(testnames, en, testfile)
     # check testfile to see whether it has 0 names in it
         notablepeople = ["Booker T. Washington", "Angie Zapata"]
-        testfile = "empty.txt"
         testresults = leftout(notablepeople, "en")
-        outputfile(testresults, testfile)
-        with codecs.open(testfile, encoding='utf-8', mode='r') as f:
+        outputfile(testresults, self.testfile)
+        with codecs.open(self.testfile, encoding='utf-8', mode='r') as f:
             testresultlength = len(f.read())
         self.assertEqual(testresultlength, 0)
 
     def test_nonexistent_page(self):
     # Check that we know a nonexistent page is nonexistent.
         imaginarypeople = ["NEVEREXISTS"]
-        testfile = "empty.txt"
         testresults = leftout(imaginarypeople, "en")
-        outputfile(testresults, testfile)
-        with codecs.open(testfile, encoding='utf-8', mode='r') as f:
+        outputfile(testresults, self.testfile)
+        with codecs.open(self.testfile, encoding='utf-8', mode='r') as f:
             testresult = f.read()
         self.assertEqual(testresult, "NEVEREXISTS\n")
 
+    def tearDown(self):
+        os.remove("empty.txt")
 
 class stats_test(unittest.TestCase):
     def test_sample_file(self):
